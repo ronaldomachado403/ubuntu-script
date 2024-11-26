f3d66868b7e104cbd9acbc225e219d5973e8e0eb
ronaldomachado403 <ronaldomachado403@gmail.com>
2024-11-26 13:13:23 -0300

    Okok

---
+++ postinstall.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt upgrade
+sudo apt install build-essential default-jdk libssl-dev exuberant-ctags ncurses-term ack-grep silversearcher-ag fontconfig imagemagick libmagickwand-dev software-properties-common git vim-gtk3 curl keepassxc stacer htop ufw neofetch zsh terminator gnome-tweaks fonts-hack-ttf vlc gedit
